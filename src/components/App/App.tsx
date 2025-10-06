7053ece89d460895ad144bed8822c10279fc7de1
helen-akateva <akateva17@gmail.com>
2025-10-06 13:38:00 +0300

    update

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,2 +1,20 @@
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import Loader from '../Loader/Loader';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import MovieModal from '../MovieModal/MovieModal';
+import SearchBar from '../SearchBar/SearchBar';
 import css from './App.module.css';
 import { useState } from "react";
+
+
+
+
+export default function App() {return (
+    <div className={css.app}>
+        <SearchBar />
+        < MovieGrid />
+        < Loader />
+        < ErrorMessage />
+        < MovieModal />
+        
+    </div>)}
