7f19ffce273da465d828284cc6f99e52871f3944
helen-akateva <akareva17@gmail.com>
2025-10-08 08:36:14 +0300

    fix

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -26,10 +26,7 @@ export default function MovieModal({ movie, onClose }: MovieModalProps) {
   const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.currentTarget === e.target) onClose();
   };
-  const imageUrl = movie.backdrop_path
-    ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
-    : `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-  
+
   return createPortal(
     <div
       className={css.backdrop}
@@ -46,7 +43,11 @@ export default function MovieModal({ movie, onClose }: MovieModalProps) {
           &times;
         </button>
 
-        <img src={imageUrl} alt={movie.title} className={css.image} />
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          alt={movie.title}
+          className={css.image}
+        />
 
         <div className={css.content}>
           <h2>{movie.title}</h2>
