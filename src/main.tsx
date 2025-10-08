531bec693ff591b396af086e9c48d8d668b970b2
helen-akateva <akateva17@gmail.com>
2025-10-08 07:54:41 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import "./index.css";
 import App from "./components/App/App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
